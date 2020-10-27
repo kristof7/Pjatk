baba0276c89109981259e358d7b6d27e6ce0909f
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2020-10-27 21:59:49 +0100

    Create S23813_zestaw1 class, add methods and implementation to first exercise

--- S23813/src/main/java/zadanie_1/Main.java
+++ S23813/src/main/java/zadanie_1/Main.java
@@ -1,4 +1,9 @@
 package zadanie_1;
 
 public class Main {
+
+    public static void main(String[] args) {
+        S23813_zestaw1 s23813_zestaw1 = new S23813_zestaw1();
+        s23813_zestaw1.cwiczenie_01();
+    }
 }
---
+++ S23813/src/main/java/zadanie_1/S23813_zestaw1.java
@@ -0,0 +1,23 @@
+package zadanie_1;
+
+public class S23813_zestaw1 {
+
+    public void cwiczenie_01() {
+        int a = 165;
+        for (int i = 15; i >= 0; i--) {
+            if ((a & 0b1 << i) != 0b0)
+                System.out.print("1");
+            else
+                System.out.print("0");
+        }
+    }
+
+    public void cwiczenie_02() {
+    }
+
+    public void cwiczenie_03() {
+    }
+
+    public void cwiczenie_04() {
+    }
+}
