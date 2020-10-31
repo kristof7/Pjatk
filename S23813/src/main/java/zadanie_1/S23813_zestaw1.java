a6aaf2ab92af5252f9fa1546e340eb904106ea39
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2020-10-31 14:44:31 +0100

    solution added to cwiczenie_04 method

--- S23813/src/main/java/zadanie_1/S23813_zestaw1.java
+++ S23813/src/main/java/zadanie_1/S23813_zestaw1.java
@@ -19,15 +19,15 @@ public class S23813_zestaw1 {
         System.out.println("Dla liczby " + a + " binarnej:");
         System.out.println("");
         System.out.print(" Pierwsze przesunięcie będzie wyglądało następująco: ");
-        System.out.println(Integer.toBinaryString(a) + ">>" + 0 + " = " + Integer.toBinaryString(a >>= 0));
+        System.out.println(Integer.toBinaryString(a) + ">>" + 1 + " = " + Integer.toBinaryString(a >>= 1));
         System.out.print(" Kolejne przesunięcie będzie wyglądało tak: ");
         System.out.println(Integer.toBinaryString(a) + "<<" + 2 + " = " + Integer.toBinaryString(a <<= 2));
         System.out.print(" Ostatnie przesunięcie wygląda tak: ");
-        System.out.println(Integer.toBinaryString(a) + ">>>" + 4 + " = " + Integer.toBinaryString(a >>>= 4));
+        System.out.println(Integer.toBinaryString(a) + ">>" + 4 + " = " + Integer.toBinaryString(a >>= 4));
         System.out.println("");
         System.out.println("Dla liczby " + b + " nie-binarnej:");
         System.out.println("");
-        System.out.println(" Pierwsze przesunięcie będzie wyglądało następująco :" + (b >> 0));
+        System.out.println(" Pierwsze przesunięcie będzie wyglądało następująco :" + (b >> 1));
         System.out.println(" Kolejne przesunięcie będzie wyglądało tak: " + (b <<= 2));
         System.out.println(" Ostatnie przesunięcie wygląda tak: " + (b >>= 4));
     }
@@ -37,5 +37,13 @@ public class S23813_zestaw1 {
     }
 
     public void cwiczenie_04() {
+        int zzz = 13;
+        int xxx = 15;
+        int yyy = 6;
+        int a1 = 0x6D, a2 = zzz, a = a1 & a2;
+        int b1 = 0xA, b2 = xxx, b = b1 | b2;
+        int c1 = 11 << 2, c2 = yyy, c = c1 ^ c2;
+
+        System.out.println(a + " " + b + " " + c);
     }
 }
