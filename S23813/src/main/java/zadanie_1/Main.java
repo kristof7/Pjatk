0c82ab501e6ed16652be1b1d574833f3f8cfb140
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2020-10-31 12:30:28 +0100

    solution added to cwiczenie_02 method, cwiczenie_01 updated

--- S23813/src/main/java/zadanie_1/Main.java
+++ S23813/src/main/java/zadanie_1/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
         S23813_zestaw1 s23813_zestaw1 = new S23813_zestaw1();
         s23813_zestaw1.cwiczenie_01();
-//        s23813_zestaw1.convertTo(5,2);
+        System.out.println("\n------------------------------------------------\n");
+        s23813_zestaw1.cwiczenie_02();
+        System.out.println("\n------------------------------------------------\n");
+        s23813_zestaw1.cwiczenie_03();
+        System.out.println("\n------------------------------------------------\n");
+        s23813_zestaw1.cwiczenie_04();
     }
 }
--- S23813/src/main/java/zadanie_1/S23813_zestaw1.java
+++ S23813/src/main/java/zadanie_1/S23813_zestaw1.java
@@ -6,36 +6,34 @@ public class S23813_zestaw1 {
 
 
     public void cwiczenie_01() {
-        int n2 = 165;
-        int n8 = 165;
-        int n16 = 165;
-        int base2 = 2;
-        int base8 = 8;
-        int base16 = 16;
-        String result2 = "";
-        String result8 = "";
-        String result16 = "";
-        while (n2 > 0) {
-            result2 = pattern.charAt(n2 % base2) + result2;
-            n2 /= base2;
-        }
-        while (n8 > 0) {
-            result8 = pattern.charAt(n8 % base8) + result8;
-            n8 /= base8;
-        }
-        while (n16 > 0) {
-            result16 = pattern.charAt(n16 % base16) + result16;
-            n16 /= base16;
-        }
-        System.out.println("165 w systemie dwujkowym = " + result2);
-        System.out.println("165 w systemie ósemkowym = " + result8);
-        System.out.println("165 w systemie szestnastkowym = " + result16);
+        int number = 165;
+
+        System.out.println("165 w systemie dwujkowym = " + Integer.toBinaryString(number));
+        System.out.println("165 w systemie ósemkowym = " + Integer.toOctalString(number));
+        System.out.println("165 w systemie szestnastkowym = " + Integer.toHexString(number));
     }
 
     public void cwiczenie_02() {
+        int a = 14;
+        int b = 14;
+        System.out.println("Dla liczby " + a + " binarnej:");
+        System.out.println("");
+        System.out.print(" Pierwsze przesunięcie będzie wyglądało następująco: ");
+        System.out.println(Integer.toBinaryString(a) + ">>" + 0 + " = " + Integer.toBinaryString(a >>= 0));
+        System.out.print(" Kolejne przesunięcie będzie wyglądało tak: ");
+        System.out.println(Integer.toBinaryString(a) + "<<" + 2 + " = " + Integer.toBinaryString(a <<= 2));
+        System.out.print(" Ostatnie przesunięcie wygląda tak: ");
+        System.out.println(Integer.toBinaryString(a) + ">>>" + 4 + " = " + Integer.toBinaryString(a >>>= 4));
+        System.out.println("");
+        System.out.println("Dla liczby " + b + " nie-binarnej:");
+        System.out.println("");
+        System.out.println(" Pierwsze przesunięcie będzie wyglądało następująco :" + (b >> 0));
+        System.out.println(" Kolejne przesunięcie będzie wyglądało tak: " + (b <<= 2));
+        System.out.println(" Ostatnie przesunięcie wygląda tak: " + (b >>= 4));
     }
 
     public void cwiczenie_03() {
+
     }
 
     public void cwiczenie_04() {
