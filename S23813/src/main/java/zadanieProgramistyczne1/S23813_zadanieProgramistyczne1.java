ee865076a34ff49d80b7e48eab8cfb4f62867ab5
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2020-11-24 23:39:53 +0100

    fix code in S23813_zadanieProgramistyczne1 class

--- S23813/src/main/java/zadanieProgramistyczne1/S23813_zadanieProgramistyczne1.java
+++ S23813/src/main/java/zadanieProgramistyczne1/S23813_zadanieProgramistyczne1.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class S23813_zadanieProgramistyczne1 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\nProgram do obliczania a-tej liczby znaczącej ilorazu dwóch liczb b/c. Zostaniesz poproszony/poproszona o wprowadzenie trzech liczb a,b,c.\n");
@@ -18,20 +17,22 @@ public class S23813_zadanieProgramistyczne1 {
         double operation = (double) b / c;
         int[] result = new int[a];
         int count = 0;
+        String regex = "[1-9]";
 
         System.out.println("Wynik dzielenia dla liczb " + b + "/" + c + " wynosi: " + operation);
 
-        String[] stringTable = String.format("%.10f", operation).replace(",", "").split("");
+        String[] stringTable = String.valueOf(operation).replace(".", "").split("");
 
         for (int i = 0; i < stringTable.length; i++) {
-            if (stringTable[i].matches("[1-9]")) {
-                result[i] = Integer.parseInt(stringTable[i]);
+            if (stringTable[i].matches(regex)) {
+                result[count] = Integer.parseInt(stringTable[i]);
+                regex = "[0-9]";
                 count++;
                 if (count == a) {
                     break;
                 }
             }
         }
-        System.out.println("a-ta cyfra znacząca dla " + b + "/" + c + " wynosi: " + result[a - 1]);
+        System.out.println("\na-ta cyfra znacząca dla " + b + "/" + c + " wynosi: " + result[a - 1]);
     }
 }
