531814d639cecb661bd6a482e5c9de22561bec34
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2020-11-24 23:55:59 +0100

    prepare template for zadanie_6 tasks

---
+++ S23813/src/main/java/zadanie_6/S23813_zestaw6.java
@@ -0,0 +1,56 @@
+package zadanie_6;
+
+public class S23813_zestaw6 {
+    public static void main(String[] args) {
+        S23813_zestaw6 s23813_zestaw6 = new S23813_zestaw6();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw6.cwiczenie_01();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw6.cwiczenie_02();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw6.cwiczenie_03();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw6.cwiczenie_04();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw6.cwiczenie_05();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw6.cwiczenie_06();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw6.cwiczenie_07();
+    }
+
+    public void cwiczenie_01() {
+        System.out.println("Cwiczenie_01\n");
+
+    }
+
+    public void cwiczenie_02() {
+        System.out.println("Cwiczenie_02\n");
+
+    }
+
+    public void cwiczenie_03() {
+        System.out.println("Cwiczenie_03\n");
+
+    }
+
+    public void cwiczenie_04() {
+        System.out.println("Cwiczenie_04\n");
+
+    }
+
+    public void cwiczenie_05() {
+        System.out.println("Cwiczenie_05\n");
+
+    }
+
+    public void cwiczenie_06() {
+        System.out.println("Cwiczenie_06\n");
+
+    }
+
+    public void cwiczenie_07() {
+        System.out.println("Cwiczenie_07\n");
+
+    }
+}
