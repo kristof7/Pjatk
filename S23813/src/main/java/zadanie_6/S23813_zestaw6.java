e490a6aef46db3e422790991e92b4171704d7267
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2020-11-25 19:23:33 +0100

    solution added to cwiczenie_03 in ...zestaw_6 class

--- S23813/src/main/java/zadanie_6/S23813_zestaw6.java
+++ S23813/src/main/java/zadanie_6/S23813_zestaw6.java
@@ -33,12 +33,12 @@ public class S23813_zestaw6 {
 
         int[] jednowymiarowa = new int[10];
 
-        for (int i = 0; i < jednowymiarowa.length ; i++) {
+        for (int i = 0; i < jednowymiarowa.length; i++) {
             int randomNum = new Random().nextInt(2);
             jednowymiarowa[i] = randomNum;
         }
-        for (int i = 0; i < jednowymiarowa.length ; i++) {
-            System.out.print(jednowymiarowa[i]+", ");
+        for (int i = 0; i < jednowymiarowa.length; i++) {
+            System.out.print(jednowymiarowa[i] + ", ");
         }
 
     }
@@ -46,6 +46,24 @@ public class S23813_zestaw6 {
     public void cwiczenie_03() {
         System.out.println("Cwiczenie_03\n");
 
+        int[] jednowymiarowa = new int[10];
+        int count0 = 0;
+        int count1 = 0;
+
+        for (int i = 0; i < jednowymiarowa.length; i++) {
+            int randomNum = new Random().nextInt(2);
+            if (randomNum == 0)
+                count0++;
+            if (randomNum == 1)
+                count1++;
+            jednowymiarowa[i] = randomNum;
+        }
+        System.out.println("Liczby w tablicy:");
+        for (int i = 0; i < jednowymiarowa.length; i++) {
+            System.out.print(jednowymiarowa[i] + ", ");
+        }
+        System.out.println("\nliczba zer[0] w tablicy: " + count0);
+        System.out.println("liczba jedynek[1] w tablicy: " + count1);
     }
 
     public void cwiczenie_04() {
