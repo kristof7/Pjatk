7368f33bea620f1b1f15a740a4ca20ad57a5c2ca
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2020-12-20 16:27:26 +0100

    solution added to cwiczenie_03 in ...zestaw_8 class

--- S23813/src/main/java/zadanie_8/S23813_zestaw8.java
+++ S23813/src/main/java/zadanie_8/S23813_zestaw8.java
@@ -5,12 +5,19 @@ public class S23813_zestaw8 {
     public static void main(String[] args) {
         S23813_zestaw8 s23813_zestaw8 = new S23813_zestaw8();
         System.out.println("\n------------------------------------------------");
+
         s23813_zestaw8.show(7);
+
         System.out.println("\n------------------------------------------------");
+
         Integer wrt = 7;
         s23813_zestaw8.modifyValue(wrt);
+
         System.out.println("\n------------------------------------------------");
-        s23813_zestaw8.cwiczenie_03();
+
+        char[] tablica = {'A', 'l', 'a', ' ', 'm', 'a', ' ', 'k', 'o', 't', 'a'};
+        s23813_zestaw8.charCount(tablica);
+
         System.out.println("\n------------------------------------------------");
         s23813_zestaw8.cwiczenie_04();
         System.out.println("\n------------------------------------------------");
@@ -25,15 +32,20 @@ public class S23813_zestaw8 {
 
     public void modifyValue(Integer wrt) {
         System.out.println("Cwiczenie_02\n");
-        System.out.println("wartość dostarczanej zmiennej= "+wrt);
+        System.out.println("wartość dostarczanej zmiennej= " + wrt);
         wrt *= 5;
-        System.out.println("wartość dostarczanej zmiennej pomnożone * 5= "+wrt);
+        System.out.println("wartość dostarczanej zmiennej pomnożone * 5= " + wrt);
 
         //metoda modyfikuje dostarczaną wqartość jeżeli taki był zamierzony cel/rezultat który programista chciał uzyskać
     }
 
-    public void cwiczenie_03() {
+    public void charCount(char[] table) {
         System.out.println("Cwiczenie_03\n");
+        Integer count = 0;
+        for (int i = 0; i < table.length; i++) {
+            count++;
+        }
+        System.out.println("Liczba znaków w argumencie= " + count);
     }
 
     public void cwiczenie_04() {
