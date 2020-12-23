3e7a0fe8949e09fd809da150deb27e5cb6203329
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2020-12-24 00:12:33 +0100

    prepare template for zadanie_9 tasks

---
+++ S23813/src/main/java/zadanie_9/S23813_zestaw9.java
@@ -0,0 +1,43 @@
+package zadanie_9;
+
+public class S23813_zestaw9 {
+
+    public static void main(String[] args) {
+        S23813_zestaw9 s23813_zestaw9 = new S23813_zestaw9();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw9.cwiczenie_01();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw9.cwiczenie_02();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw9.cwiczenie_03();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw9.cwiczenie_04();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw9.cwiczenie_05();
+
+    }
+
+    public void cwiczenie_01() {
+        System.out.println("Cwiczenie_01\n");
+
+    }
+
+    public void cwiczenie_02() {
+        System.out.println("Cwiczenie_02\n");
+
+    }
+
+    public void cwiczenie_03() {
+        System.out.println("Cwiczenie_03\n");
+
+    }
+
+    public void cwiczenie_04() {
+        System.out.println("Cwiczenie_04\n");
+    }
+
+    public void cwiczenie_05() {
+        System.out.println("Cwiczenie_05\n");
+    }
+
+}
