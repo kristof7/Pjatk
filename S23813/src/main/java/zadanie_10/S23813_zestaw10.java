762bc96aa2281b29db4dc7f1c9c451803136bd93
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-01-12 19:23:26 +0100

    prepare template for zadanie_10 tasks

---
+++ S23813/src/main/java/zadanie_10/S23813_zestaw10.java
@@ -0,0 +1,45 @@
+package zadanie_10;
+
+public class S23813_zestaw10 {
+	public static void main(String[] args) {
+		S23813_zestaw10 s23813_zestaw10=new S23813_zestaw10();
+		System.out.println("\n------------------------------------------------");
+		s23813_zestaw10.cwiczenie_01();
+		System.out.println("\n------------------------------------------------");
+		s23813_zestaw10.cwiczenie_02();
+		System.out.println("\n------------------------------------------------");
+		s23813_zestaw10.cwiczenie_03();
+		System.out.println("\n------------------------------------------------");
+		s23813_zestaw10.cwiczenie_04();
+		System.out.println("\n------------------------------------------------");
+		s23813_zestaw10.cwiczenie_05();
+
+	}
+
+	public void cwiczenie_01() {
+		System.out.println("Cwiczenie_01\n");
+
+	}
+
+	public void cwiczenie_02() {
+		System.out.println("Cwiczenie_02\n");
+
+	}
+
+	public void cwiczenie_03() {
+		System.out.println("Cwiczenie_03\n");
+
+	}
+
+	public void cwiczenie_04() {
+		System.out.println("Cwiczenie_04\n");
+
+	}
+
+	public void cwiczenie_05() {
+		System.out.println("Cwiczenie_05\n");
+
+	}
+
+
+}
