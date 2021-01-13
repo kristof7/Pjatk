10fb672ef215accb6de14da9fbb8b2b67408f626
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-01-13 23:56:57 +0100

    ...zadanieProgramistyczne2 class update - (field weight added), renamed field "data" to "name"

--- S23813/src/main/java/zadanieProgramistyczne2/S23813_zadanieProgramistyczne2.java
+++ S23813/src/main/java/zadanieProgramistyczne2/S23813_zadanieProgramistyczne2.java
@@ -7,32 +7,33 @@ public class S23813_zadanieProgramistyczne2 {
 
 	public static void main(String[] args) {
 
+		Node<String> parent = new Node<String>("A", 2);
+		Node<String> child1 = parent.addChild(new Node<String>("B",10));
+		Node<String> child2 = parent.addChild(new Node<String>("C", 12));
+		Node<String> child3 = parent.addChild(new Node<String>("D", 7));
+		Node<String> child11 = child1.addChild(new Node<String>("E", 3));
+		Node<String> child31 = child3.addChild(new Node<String>("F", 5));
+		Node<String> child32 = child3.addChild(new Node<String>("G", 9));
 
-		Node<String> parent = new Node<String>("A");
-		Node<String> child1 = parent.addChild(new Node<String>("B"));
-		Node<String> child2 = parent.addChild(new Node<String>("C"));
-		Node<String> child3 = parent.addChild(new Node<String>("D"));
-		Node<String> child11 = child1.addChild(new Node<String>("E"));
-		Node<String> child31 = child3.addChild(new Node<String>("F"));
-		Node<String> child32 = child3.addChild(new Node<String>("G"));
-
-		System.out.println(parent.getData());
-		System.out.println(child1.getData());
-		System.out.println(child31.getData());
-
+		System.out.println(parent.getName() +" "+ parent.getWeight());
+		System.out.println(child1.getName() +" "+ child1.getWeight());
+		System.out.println(child31.getName() +" "+ child31.getWeight());
 
 	}
 }
 
 class Node<T> {
 
-	private T data = null;
+	private T name;
+	private int weight;
 	private List<Node<T>> children = new ArrayList<>();
 	private Node<T> parent = null;
 
-	public Node(T data) {
-		this.data=data;
+	public Node(T name, int weight) {
+		this.name=name;
+		this.weight=weight;
 	}
+
 	public Node<T> addChild(Node<T> child) {
 		child.setParent(this);
 		this.children.add(child);
@@ -52,12 +53,16 @@ class Node<T> {
 		return children;
 	}
 
-	public T getData() {
-		return data;
+	public T getName() {
+		return name;
 	}
 
-	public void setData(T data) {
-		this.data=data;
+	public void setName(T name) {
+		this.name=name;
+	}
+
+	public int getWeight() {
+		return weight;
 	}
 }
 
