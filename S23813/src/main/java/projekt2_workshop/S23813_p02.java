d41373a3819fe025ac8c3a70aaa4527cd7787c69
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-01-17 20:51:41 +0100

    prepare template for projekt2_workshop project

---
+++ S23813/src/main/java/projekt2_workshop/S23813_p02.java
@@ -0,0 +1,24 @@
+package projekt2_workshop;
+
+public class S23813_p02 {
+	public static void main(String[] args) {
+
+
+	}
+}
+
+class Bolt {
+
+}
+
+class Screw {
+
+}
+
+class Nut {
+
+}
+
+class Pad {
+
+}
