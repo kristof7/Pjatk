21890101b40e63b057c4efe77d3d6f248d8e0239
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-01-26 14:22:24 +0100

    template prepared to ...zadanieProgramistyczne3

---
+++ S23813/src/main/java/zadanieProgramistyczne3/S23813_zadanieProgramistyczne3.java
@@ -0,0 +1,9 @@
+package zadanieProgramistyczne3;
+
+public class S23813_zadanieProgramistyczne3 {
+    public static void main(String[] args) {
+        int inputA = 6;
+        int inputB = 14;
+
+    }
+}
