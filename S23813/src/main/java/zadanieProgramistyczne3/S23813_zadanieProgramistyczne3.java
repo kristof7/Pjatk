a42495c26b18aa26e17af8da51f4c8d4e696b236
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-01-26 14:51:17 +0100

    Result added to ...zadanieProgramistyczne3 (Least common multiple)

--- S23813/src/main/java/zadanieProgramistyczne3/S23813_zadanieProgramistyczne3.java
+++ S23813/src/main/java/zadanieProgramistyczne3/S23813_zadanieProgramistyczne3.java
@@ -2,8 +2,26 @@ package zadanieProgramistyczne3;
 
 public class S23813_zadanieProgramistyczne3 {
     public static void main(String[] args) {
-        int inputA = 6;
-        int inputB = 14;
+        int inputA = 135;
+        int inputB = 60;
+        System.out.println("Input a: "+ inputA);
+        System.out.println("Input b: "+ inputB);
+
+        System.out.println("Least common multiple: "+ lcm(inputA, inputB));
 
     }
+
+    public static int lcm(int a, int b) {
+        Integer result = a * b / gcd_recursive(a, b);
+
+        return result;
+    }
+
+    public static Integer gcd_recursive(int a, int b) {
+        if (b != 0)
+            return gcd_recursive(b, a % b);
+        else
+            return a;
+    }
+
 }
