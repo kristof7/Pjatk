9e354b0d4ecb9e642c99685a6498829d19c7e105
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-01-27 12:06:54 +0100

    template prepared for zadanie_11 tasks

---
+++ S23813/src/main/java/zadanie_11/S23813_zestaw11.java
@@ -0,0 +1,30 @@
+package zadanie_11;
+
+public class S23813_zestaw11 {
+
+    public static void main(String[] args) {
+        S23813_zestaw11 s23813_zestaw11 = new S23813_zestaw11();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw11.cwiczenie_01();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw11.cwiczenie_02();
+        System.out.println("\n------------------------------------------------");
+        s23813_zestaw11.cwiczenie_03();
+
+    }
+
+    public void cwiczenie_01() {
+        System.out.println("Cwiczenie_01\n");
+
+    }
+
+    public void cwiczenie_02() {
+        System.out.println("Cwiczenie_02\n");
+
+    }
+
+    public void cwiczenie_03() {
+        System.out.println("Cwiczenie_03\n");
+
+    }
+}
