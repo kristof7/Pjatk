964b46700d7cf60fbaf58218100cb1431abe1ee7
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-02-04 17:16:52 +0100

    template prepared for projekt3_zwierzeta task

---
+++ S23813/src/main/java/projekt3_zwierzeta/S23813_p03.java
@@ -0,0 +1,28 @@
+package projekt3_zwierzeta;
+
+public class S23813_p03 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Ssak {
+    private String imie;
+    private short rokUrodzenia;
+    private boolean mlody;
+
+}
+
+class Wadera extends Ssak {
+    private int iloscSzczeniat;
+    private Ssak[] szczenieta;
+
+}
+
+class Wilk extends Ssak {
+    private String nazwaStada;
+    private int pozycja;
+
+}
+
+
