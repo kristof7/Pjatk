5d5e47c0a464ec274382ea17cfd204cb15bbe38a
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-02-05 22:24:54 +0100

    projekt3_zwierzeta project update, Serializable implementation added, zaladuj & zapisz methods logic has been improved

--- S23813/src/main/java/projekt3_zwierzeta/S23813_p03.java
+++ S23813/src/main/java/projekt3_zwierzeta/S23813_p03.java
@@ -1,31 +1,29 @@
 package projekt3_zwierzeta;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class S23813_p03 {
 
-    private static String fileName = "wilki.txt";
+    private static String filePath = "wilki.txt";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        Ssak ssak = new Ssak();
-        Wadera wadera7 = new Wadera("WaderaMloda", (short) 2021, true, 0, null);
-        Wadera wadera8 = new Wadera("WaderaMloda2", (short) 2020, true, 0, null);
-        Wilk wilk7 = new Wilk("WilkMlody", (short) 2020, true, "stadoA", 4);
-        Wilk wilk8 = new Wilk("WilkMlody2", (short) 2021, true, "stadoB", 3);
-
-        Ssak[] szczenieta1 = {wadera7, wilk7};
-        Ssak[] szczenieta2 = {wadera8};
-        Ssak[] szczenieta3 = {wilk8};
+        Ssak[] szczenieta = {};
+        Wadera waderaMloda1 = new Wadera("WaderaMloda", (short) 2021, true, 0, szczenieta);
+        Wadera waderaMloda2 = new Wadera("WaderaMloda2", (short) 2020, true, 0, szczenieta);
+        Wilk wilkMlody1 = new Wilk("WilkMlody", (short) 2020, true, "stadoA", 4);
+        Wilk wilkMlody2 = new Wilk("WilkMlody2", (short) 2021, true, "stadoB", 3);
+        Ssak[] szczenieta1 = {waderaMloda1, wilkMlody1};
+        Ssak[] szczenieta2 = {waderaMloda2};
+        Ssak[] szczenieta3 = {wilkMlody2};
 
         Wadera wadera1 = new Wadera("Wadera1", (short) 2014, false, 2, szczenieta1);
         Wadera wadera2 = new Wadera("Wadera2", (short) 2015, false, 1, szczenieta2);
-        Wadera wadera3 = new Wadera("Wadera3", (short) 2016, false, 0, null);
-        Wadera wadera4 = new Wadera("Wadera4", (short) 2017, false, 0, null);
+        Wadera wadera3 = new Wadera("Wadera3", (short) 2016, false, 0, szczenieta);
+        Wadera wadera4 = new Wadera("Wadera4", (short) 2017, false, 0, szczenieta);
         Wadera wadera5 = new Wadera("Wadera5", (short) 2018, false, 1, szczenieta3);
-        Wadera wadera6 = new Wadera("Wadera5", (short) 2013, false, 0, null);
+        Wadera wadera6 = new Wadera("Wadera5", (short) 2013, false, 0, szczenieta);
 
         Wilk wilk1 = new Wilk("Wilk1", (short) 2012, false, "stadoA", 1);
         Wilk wilk2 = new Wilk("Wilk2", (short) 2013, false, "stadoB", 1);
@@ -35,35 +33,52 @@ public class S23813_p03 {
         Wilk wilk6 = new Wilk("Wilk6", (short) 2011, false, "stadoB", 3);
 
 
-        Ssak[] stadoA = {wilk1, wilk3, wilk5, wadera1, wadera3, wadera5, wadera7, wilk7};
-        Ssak[] stadoB = {wilk2, wilk4, wilk6, wadera2, wadera4, wadera6, wadera8, wilk8};
+        Ssak[] stadoA = {wilk1, wilk3, wilk5, wadera1, wadera3, wadera5, waderaMloda1, wilkMlody1};
+        Ssak[] stadoB = {wilk2, wilk4, wilk6, wadera2, wadera4, wadera6, waderaMloda2, wilkMlody2};
 
-        File file = new File(fileName);
+        File file = new File(filePath);
 
-        if (file.exists() && !file.isDirectory() && file.length() > 0) {
-            ssak.zaladuj(fileName);
+        if (file.exists() && !file.isDirectory() & file.length() > 0) {
+            zaladuj(filePath);
         } else {
-            try (FileOutputStream fos = new FileOutputStream(fileName)) {
+            try (FileOutputStream fos = new FileOutputStream(filePath)) {
 
-                String text = " ----- STADO A ---- ";
-                byte[] stadoAText = text.getBytes();
-                String text2 = " ----- STADO B ---- ";
-                byte[] stadoBText = text2.getBytes();
-
-                fos.write(stadoAText);
                 for (Ssak s : stadoA) {
                     s.zapisz(fos);
                 }
-                fos.write(stadoBText);
+
                 for (Ssak s : stadoB) {
                     s.zapisz(fos);
                 }
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
+
+    public static Ssak[] zaladuj(String sciezka) {
+
+        try (FileInputStream fileInputStream = new FileInputStream(sciezka)) {
+
+            ObjectInputStream objStream;
+            try {
+                do {
+                    objStream = new ObjectInputStream(fileInputStream);
+                    Object o = objStream.readObject();
+                    System.out.println(o);
+                } while (true);
+            } catch (EOFException e) {
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
 
-class Ssak {
+class Ssak implements Serializable {
     private String imie;
     private short rokUrodzenia;
     private boolean mlody;
@@ -74,10 +89,6 @@ class Ssak {
         this.mlody = mlody;
     }
 
-    public Ssak() {
-
-    }
-
     public String przedstawSie() {
         return "{" +
                 "imie='" + imie + '\'' +
@@ -86,25 +97,21 @@ class Ssak {
                 '}';
     }
 
-    void zapisz(FileOutputStream fileOutputStream) {
-        try {
-            byte[] array = przedstawSie().getBytes(StandardCharsets.UTF_8);
-            fileOutputStream.write(array);
-        } catch (Exception e) {
-            e.getStackTrace();
-        }
+    @Override
+    public String toString() {
+        return "Ssak{" +
+                "imie='" + imie + '\'' +
+                ", rokUrodzenia=" + rokUrodzenia +
+                ", mlody=" + mlody +
+                '}';
     }
 
-    void zaladuj(String sciezka) {
-
-        File file = new File(sciezka);
-        try (FileInputStream fileInputStream = new FileInputStream(file)) {
-            int line;
-            while ((line = fileInputStream.read()) != -1) {
-                System.out.print((char) line);
-            }
+    void zapisz(FileOutputStream fileOutputStream) {
+        try {
+            ObjectOutputStream object = new ObjectOutputStream(fileOutputStream);
+            object.writeObject(this.przedstawSie());
         } catch (Exception e) {
-            e.printStackTrace();
+            e.getStackTrace();
         }
     }
 }
